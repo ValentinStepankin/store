19438e952c98e2c7d4182e7a95088c04096b9723
Valentin Stepankin <vallentinnnnn546@gmail.com>
2024-07-22 18:11:26 +0300

    ---

--- users/views.py
+++ users/views.py
@@ -33,6 +33,14 @@ def registration(request):
     return render(request, 'users/registration.html', context)
 
 def profile(request):
-    form = UserProfileForm(instance=request.user)
+    if request.method == 'POST':
+        form = UserProfileForm(instance=request.user, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:profile'))
+        else:
+            print(form.errors)
+    else:
+        form = UserProfileForm(instance=request.user)
     context = {'title': 'Store - профиль', 'form': form}
     return render(request, 'users/profile.html', context)
